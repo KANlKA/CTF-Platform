d26d571bf8b8ea15f1652ef0b8a9674a92cc13dc
kanika <155656637+KANlKA@users.noreply.github.com>
2025-05-04 00:36:01 +0530

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   testEnvironment: 'node',
   testTimeout: 30000,
-  setupFilesAfterEnv: ['./tests/testSetup.js'],
+  globalSetup: './tests/testSetup.js',
+  setupFilesAfterEnv: ['./tests/testSetup.js'], // For the clearDatabase function
   testPathIgnorePatterns: ['/node_modules/'],
-  verbose: true,
-  detectOpenHandles: true,
-  forceExit: true,
-  testEnvironmentOptions: {
-    NODE_ENV: 'test'
-  }
+  verbose: true
 };
