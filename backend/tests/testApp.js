d2747d5171867ac2ca58118d6ca84e1567dd7b33
kanika <155656637+KANlKA@users.noreply.github.com>
2025-05-04 04:22:50 +0530

    Update testApp.js

--- backend/tests/testApp.js
+++ backend/tests/testApp.js
@@ -1,29 +1,12 @@
 // testApp.js
 const express = require('express');
-const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
 const routes = require('../routes');
 
 async function createTestApp() {
   const app = express();
-  
-  // Start in-memory MongoDB
-  const mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
-
-  // Connect to MongoDB
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-
-  // Middleware
   app.use(express.json());
-  
-  // Routes
   app.use('/', routes);
-
-  return { app, mongoServer };
+  return app;
 }
 
 module.exports = createTestApp;
