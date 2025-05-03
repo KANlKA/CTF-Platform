3d16192370bd33b1510ae3ab5da6226a2f91f1c5
kanika <155656637+KANlKA@users.noreply.github.com>
2025-05-04 01:14:18 +0530

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   testTimeout: 30000,
   globalSetup: './tests/testSetup.js',
+  setupFiles: ['dotenv/config'],
   setupFilesAfterEnv: ['./tests/testSetup.js'], // For the clearDatabase function
   testPathIgnorePatterns: ['/node_modules/'],
   modulePaths: ['<rootDir>'],
