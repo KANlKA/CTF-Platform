4239a8ccefe3d27b798b488b37f235da17d82b0c
kanika <155656637+KANlKA@users.noreply.github.com>
2025-05-04 00:06:29 +0530

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,12 +2,10 @@ module.exports = {
   testEnvironment: 'node',
   testTimeout: 30000, 
   setupFilesAfterEnv: ['./tests/testSetup.js'],
-  globalSetup: './tests/globalSetup.js',
-  globalTeardown: './tests/globalTeardown.js',
   testPathIgnorePatterns: ['/node_modules/'],
   detectOpenHandles: true,
   forceExit: true,
   testEnvironmentOptions: {
-    NODE_ENV: 'test'
+  NODE_ENV: 'test'
   }
 };
