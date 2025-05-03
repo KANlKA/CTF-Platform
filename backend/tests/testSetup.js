8491fff48a678f5c160030de68ee9cce7df9991e
kanika <155656637+KANlKA@users.noreply.github.com>
2025-05-03 23:43:01 +0530

    Update testSetup.js

--- backend/tests/testSetup.js
+++ backend/tests/testSetup.js
@@ -1,19 +1,8 @@
 const mongoose = require('mongoose');
 
-beforeAll(async () => {
+module.exports = async () => {
   await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
-}, 30000);
-
-afterAll(async () => {
-  await mongoose.disconnect();
-});
-
-beforeEach(async () => {
-  const collections = mongoose.connection.collections;
-  for (const key in collections) {
-    await collections[key].deleteMany({});
-  }
-});
+};
