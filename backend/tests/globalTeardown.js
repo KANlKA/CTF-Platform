b68b830ec893df362744ce85c7c72c8ffe61851a
kanika <155656637+KANlKA@users.noreply.github.com>
2025-05-03 23:43:20 +0530

    Update globalTeardown.js

--- backend/tests/globalTeardown.js
+++ backend/tests/globalTeardown.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose');
 
-module.exports = async function () {
-  // Close all connections
+module.exports = async () => {
   await mongoose.disconnect();
-  
-  // Stop in-memory server
-  if (global.__MONGO_SERVER__) {
-    await global.__MONGO_SERVER__.stop();
-  }
-  
-  // Force exit
-  await new Promise(resolve => setTimeout(resolve, 1000));
 };
